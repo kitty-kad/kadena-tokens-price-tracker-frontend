a3a7eb3ed8710c143426f2267b7894d7db56a834
kitty-kad <>
2022-07-25 21:50:24 +0100

    Added support for smaller values

--- src/components/price-graph.jsx
+++ src/components/price-graph.jsx
@@ -12,22 +12,37 @@ export const PriceGraph = ({
   dataCandle,
   showCandle,
 }) => {
-  const series = useMemo(() => {
+  const { series, max, exponent, multiplier } = useMemo(() => {
+    let max = 0;
     const vals = [];
     for (let i = 0; i < data.length; i++) {
       const price = showInKda ? data[i].price_in_kda : data[i].price_in_usd;
+      max = Math.max(max, price);
       vals.push([parseInt(data[i].unix_time) * 1000, price]);
     }
     // Add latest curr price entry
     if (currPrice != null && kdaToUsd != null) {
       vals.push([Date.now(), currPrice * (showInKda ? 1 : kdaToUsd)]);
     }
-    return [
-      {
-        name: `Price in ${showInKda ? "KDA" : "USD"}`,
-        data: vals,
-      },
-    ];
+
+    const { exponent, multiplier } = getExponentAndMultiplier(max);
+    if (exponent !== null) {
+      for (let i = 0; i < vals.length; i++) {
+        vals[i][1] = vals[i][1] * multiplier;
+      }
+    }
+
+    return {
+      series: [
+        {
+          name: `Price in ${showInKda ? "KDA" : "USD"}`,
+          data: vals,
+        },
+      ],
+      max,
+      exponent,
+      multiplier,
+    };
   }, [data, currPrice, kdaToUsd, showInKda]);
 
   const candleSeries = useMemo(() => {
@@ -50,13 +65,19 @@ export const PriceGraph = ({
           candleData.price_in_usd_end,
         ];
       }
+
+      if (multiplier !== null) {
+        for (let i = 0; i < priceArr.length; i++) {
+          priceArr[i] = priceArr[i] * multiplier;
+        }
+      }
       vals.push({
         x: new Date(candleData.unix_time * 1000),
         y: priceArr,
       });
     });
     return [{ data: vals }];
-  }, [dataCandle, showInKda]);
+  }, [dataCandle, showInKda, multiplier]);
 
   const chartRef = useRef(null);
   const candeChartRef = useRef(null);
@@ -66,10 +87,10 @@ export const PriceGraph = ({
   useMemo(() => {
     const chart = chartRef?.current?.chart;
     const candleChart = candeChartRef?.current?.chart;
-    chart?.updateOptions({ yaxis: getYOptions(showInKda) });
-    candleChart?.updateOptions({ yaxis: getYOptions(showInKda) });
-    // showCandle is passed in to force a refresh when switching graphs
-  }, [showInKda, showCandle]);
+    chart?.updateOptions({ yaxis: getYOptions(exponent) });
+    candleChart?.updateOptions({ yaxis: getYOptions(exponent) });
+    // showCandle and showinKda is passed in to force a refresh when switching graphs
+  }, [showInKda, showCandle, exponent]);
 
   return (
     <div>
@@ -103,17 +124,21 @@ export const PriceGraph = ({
 
 const showIcons = window.screen.width > 500;
 
-function getYOptions(showInKda) {
+function valueFormatter(val, exponent) {
+  return `${val.toPrecision(3)}${exponent == null ? "" : `e-${exponent}`}`;
+}
+
+function getYOptions(exponent) {
   return {
     labels: {
       offsetX: -10,
-      formatter: function (val) {
-        return val.toPrecision(3);
-      },
+      formatter: (val) => valueFormatter(val, exponent),
     },
     tooltip: {
       enabled: true,
     },
+    tickAmount: 5,
+    max: (max) => max,
   };
 }
 function getGraphOptions() {
@@ -186,4 +211,22 @@ function getGraphOptions() {
       fillOpacity: 0.7,
     },
   };
+}
+
+function getExponentAndMultiplier(x) {
+  let exponent = null;
+  let multiplier = null;
+  const zeroes = zeroesAfterPoint(x);
+  if (zeroes > 4) {
+    exponent = zeroes + 1;
+    multiplier = 10 ** exponent;
+  }
+  return { exponent, multiplier };
+}
+function zeroesAfterPoint(x) {
+  if (x % 1 === 0) {
+    return 0;
+  } else {
+    return -1 - Math.floor(Math.log10(x % 1));
+  }
 }
